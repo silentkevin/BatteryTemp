1c0dcf09c6e418537245252544cafd3660617d91
Kevin Brown <kevin.d.brown@saic.com>
2011-04-06 12:01:44 -0400

    adding license stuff

--- src/org/sksi/batterytemp/BootReceiver.java
+++ src/org/sksi/batterytemp/BootReceiver.java
@@ -1,3 +1,16 @@
+/*
+ * This source and everything with it are licensed under the Apache 2.0 license.
+ * For a copy of the license see the LICENSE file in the root of this project or
+ *   go to:  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.sksi.batterytemp;
 
 import android.app.Activity;
